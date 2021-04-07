5c9dd0df40c17b590c7ab0895960075760264e84
Jaroke321 <jakekeller321@gmail.com>
2021-04-07 14:18:18 -0400

    Completed Binary Search in Python

---
+++ BinarySearch/binarySearch.py
@@ -0,0 +1,48 @@
+import sys
+
+def main():
+    
+    # Create a makeshift ordered list
+    dummyList = [ x for x in range(1, 200, 3) ]
+    # Print out the indices of all of the values in the list to compare with binary search 
+    for idx, val in enumerate(dummyList):
+        print(str(idx) + " : " + str(val))
+
+    print() # Puts space between list and result
+
+    # Perform binary search
+    ans = binSearch(dummyList, 187)
+    # Print the index that the value was found at
+    print("The value 187 was found at index: %s" %ans)
+    
+
+def binSearch(arr, target):
+    '''This method is used to demonstrate the use of the Binary Search algorithm
+    in Python.'''
+
+    start = 0            # First iteration means we are searching entire list
+    end = len(arr) - 1   # Gets last index of array
+
+    # Go through array until value is found or return -1
+    while(start <= end):
+
+        # Calculate midpoint
+        mid =  int((start + end) / 2)
+
+        if arr[mid] == target:  # Target found
+            return mid
+        elif target < arr[mid]: # Target is on the left half of the list
+            end = mid - 1
+        else:                   # Target is on the right half of the list
+            start = mid + 1
+
+    return (-1)   # Target is not in the list
+
+    
+
+
+
+if __name__ == "__main__":
+    '''This file was not imported'''
+
+    main()  # Start program
